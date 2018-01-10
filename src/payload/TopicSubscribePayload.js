713a60b1ee5e00518befefb14093867f454584c0
itrambovetskiy <igortram@gmail.com>
2018-01-10 18:19:35 +0200

    Added subscription group field to Subscription and Unsubscription payloads

--- src/payload/TopicSubscribePayload.js
+++ src/payload/TopicSubscribePayload.js
@@ -8,24 +8,28 @@ class TopicSubscribePayload extends Payload {
 
     /**
      * Transforms raw DeviceHive message topic subscribe payload to JS object with full named fields
+     * @param sg - subscription group
      * @param t - topics list
      * @returns {TopicSubscribePayload}
      */
-    static normalize({ t } = {}) {
+    static normalize({ sg, t } = {}) {
         return new TopicSubscribePayload({
+            subscriptionGroup: sg,
             topicList: t
         });
     }
 
     /**
      * Creates new DeviceHive proxy message topic subscribe payload object
+     * @param subscriptionGroup - subscription group
      * @param topicList - topics list
      */
-    constructor({ topicList } = {}) {
+    constructor({ subscriptionGroup, topicList } = {}) {
         super();
 
         const me = this;
 
+        me.subscriptionGroup = subscriptionGroup;
         me.topicList = topicList;
     }
 
@@ -41,14 +45,27 @@ class TopicSubscribePayload extends Payload {
         me._topicList = value;
     }
 
+    get subscriptionGroup() {
+        const me = this;
+
+        return me._subscriptionGroup;
+    }
+
+    set subscriptionGroup(value) {
+        const me = this;
+
+        me._subscriptionGroup = value;
+    }
+
     /**
      * Transforms full named DeviceHive proxy message topic subscribe payload to raw proxy message
-     * @returns {{t: *}}
+     * @returns {{sg: *, t: *}}
      */
     toObject() {
         const me = this;
 
         return {
+            sg: me.subscriptionGroup,
             t: me.topicList
         }
     }
--- src/payload/TopicUnsubscribePayload.js
+++ src/payload/TopicUnsubscribePayload.js
@@ -8,24 +8,28 @@ class TopicUnsubscribePayload extends Payload {
 
     /**
      * Transforms raw DeviceHive message topic unsubscribe payload to JS object with full named fields
+     * @param sg - subscription group
      * @param t - topics list
      * @returns {TopicUnsubscribePayload}
      */
-    static normalize({ t } = {}) {
+    static normalize({ sg, t } = {}) {
         return new TopicUnsubscribePayload({
+            subscriptionGroup: sg,
             topicList: t
         });
     }
 
     /**
      * Creates new DeviceHive proxy message topic unsubscribe payload object
+     * @param subscriptionGroup - subscription group
      * @param topicList - topics list
      */
-    constructor({ topicList } = {}) {
+    constructor({ subscriptionGroup, topicList } = {}) {
         super();
 
         const me = this;
 
+        me.subscriptionGroup = subscriptionGroup;
         me.topicList = topicList;
     }
 
@@ -41,14 +45,27 @@ class TopicUnsubscribePayload extends Payload {
         me._topicList = value;
     }
 
+    get subscriptionGroup() {
+        const me = this;
+
+        return me._subscriptionGroup;
+    }
+
+    set subscriptionGroup(value) {
+        const me = this;
+
+        me._subscriptionGroup = value;
+    }
+
     /**
      * Transforms full named DeviceHive proxy message topic unsubscribe payload to raw proxy message
-     * @returns {{t: *}}
+     * @returns {{sg: *, t: *}}
      */
     toObject() {
         const me = this;
 
         return {
+            sg: me.subscriptionGroup,
             t: me.topicList
         }
     }
